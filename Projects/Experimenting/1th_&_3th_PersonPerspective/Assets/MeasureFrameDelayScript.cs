a4b0f344f8d9a49cf1f60823ee9495fbe3b502ff
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-03-20 13:57:42 +0100

    created measurefps and measureheadmovementtocsv scripts

---
+++ Projects/Experimenting/1th_&_3th_PersonPerspective/Assets/MeasureFrameDelayScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class MeasureFrameDelayScript : MonoBehaviour {
+
+    Stopwatch swIntervals = new Stopwatch();
+    long total = 0;
+    long average = 0;
+    int frameNumber = 0;
+    long highestDelay = 0;
+    long fpsToAim = 0;
+
+
+	void Start () {
+        UnityEngine.Debug.Log("Start");
+        swIntervals.Start();
+	}
+
+
+	void Update () {
+        swIntervals.Stop();
+        frameNumber++;
+
+        UnityEngine.Debug.Log("frame: " + frameNumber + " + elapsed time in ms: " + swIntervals.ElapsedMilliseconds);
+
+        total += swIntervals.ElapsedMilliseconds;
+        average = total / frameNumber;
+
+        UnityEngine.Debug.Log("average: " + average);
+
+        if (swIntervals.ElapsedMilliseconds > highestDelay)
+            highestDelay = swIntervals.ElapsedMilliseconds;
+
+        UnityEngine.Debug.Log("highestDelay: " + highestDelay);
+
+        fpsToAim = 1000 / average;
+
+        UnityEngine.Debug.Log("fpsToAim: " + fpsToAim);
+
+        swIntervals.Reset();
+        swIntervals.Start();
+    }
+}
