57095bea38609fe04e36d1a15142ffd69438fdcc
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-05-22 15:29:02 +0200

    new project sculpturebasement, wrote code for detecting locations to zoom in

---
+++ Projects/AcademyOfFineArts/SculptureRoomBasement/Assets/VideoScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoScript : MonoBehaviour {
+
+    public List<ZoomPictureLocation> zoomPictureLocations = new List<ZoomPictureLocation>();
+
+    [Serializable]
+    public class ZoomPictureLocation
+    {
+        public Vector2 Vector_ZeroPoint = new Vector2(); //zero point where raster starts
+        public Vector2 Vector_Spread = new Vector2(); //amount of dergees of turning head
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateVariables();
+        for (int i = 0; i < zoomPictureLocations.Count; i++)
+        {
+            if (detector.checkForHit(zoomPictureLocations[i].Vector_ZeroPoint, zoomPictureLocations[i].Vector_Spread, vAngle))
+                Debug.Log("object of picture to zoom on: " + i); //zoom in location
+        }
+	}
+
+    private void updateVariables()
+    {
+        //vAngle = GameObject.Find("Camera (eye)").transform.rotation.eulerAngles; //FOR STEAM VR WITH HEADSET
+        vAngle = GameObject.Find("Camera").transform.rotation.eulerAngles; //FOR SIMULATOR USING VRTK
+        //Debug.Log("vAngle X: " + vAngle.x + " Y: " + vAngle.y);
+    }
+
+    ZoomPictureDetector detector = new ZoomPictureDetector();
+    Vector3 vAngle = new Vector3();
+}
