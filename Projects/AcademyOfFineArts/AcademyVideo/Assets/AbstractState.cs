f902311c279c0978e434f35eb6d1940d022c8a30
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-05-29 13:59:12 +0200

    created project for the full video. Code for going through states works with dummystates

---
+++ Projects/AcademyOfFineArts/AcademyVideo/Assets/AbstractState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractState {
+
+    public int stateID;
+    public bool stateFinished = false;
+
+    //In this method you will have to set the bool variable 'stateFinished' when the state is finished, 
+    //so that you can switch to the next state.
+    public abstract void RunState();
+
+    //With this method we can call up if states it finished to switch to next state, 
+    //or if it still running.
+    public abstract bool IsFinished();
+
+    //With this method we will redefine all necessary variable to stop and reset the state to be able to switch to the next state and reuse this state.
+    public abstract void StopState();
+
+    //This method will be used to check if all parameters exist or if they are set properly. 
+    //This can be used to override and check for each state separate, which then can be run by the manager script to run over all methods.
+    public virtual bool CheckParameters()
+    {
+        if (!stateFinished)
+            return true;
+        else
+        {
+            UnityEngine.Debug.LogWarning("Parameter 'stateFinished' is set on 'true' before entering stage.");
+            return false;
+        }
+    }
+}
---
+++ Projects/AcademyOfFineArts/AcademyVideo/Assets/VideoScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour {
+
+    public VideoPlayer VideoPlayer;
+
+	// Use this for initialization
+	void Start () {
+        setStates();
+        checkParametersOfStates();
+        setStartState();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (currentState.IsFinished())
+        {
+            changeToNextState();
+        }
+            
+        currentState.RunState();
+	}
+
+    private void setStates()
+    {
+        states = new AbstractState[amountOfStates];
+        states[0] = new State0(0);
+        states[1] = new State1(1);
+        states[2] = new State2(2);
+        states[3] = new State3(3);
+        states[4] = new State4(4);
+    }
+    private void checkParametersOfStates()
+    {
+        for (int i = 0; i < amountOfStates; i++)
+        {
+            if (!states[i].CheckParameters())
+                Debug.LogError("Parameters of state " + states[i] + " are set wrong or are incomplete.");
+        }
+    }
+    private void setStartState()
+    {
+        currentStateNumber = 0;
+        currentState = states[currentStateNumber];
+    }
+
+    private void changeToNextState()
+    {
+        currentState.StopState();
+
+        if (currentStateNumber < amountOfStates - 1)
+            currentStateNumber++;
+        else
+            currentStateNumber = 0;
+
+        currentState = states[currentStateNumber];
+    }
+    private void startTakeNumber(int numberOfTake)
+    {
+        VideoPlayer.url = "Assets/Footage/" + numberOfTake.ToString() + ".MP4";
+        VideoPlayer.Play();
+    }
+
+    AbstractState[] states;
+    int amountOfStates = 5;
+    int currentStateNumber;
+    AbstractState currentState;
+    
+}
